19659ef341fdac0617e3adfca752c30ff706bed6
Silva, Antonio Lucas <antonio.lucas2011@hotmail.com>
2025-03-15 22:31:11 -0300

    feat: single ec2 deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region                      = "us-east-2"
   endpoints {
@@ -7,11 +20,56 @@ provider "aws" {
   }
 }
 
+variable "server_port" {
+  description = "A porta que o servidor usará para solicitações HTTP"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  description = "O endereço IP público do servidor web"
+  value = aws_instance.example.public_ip
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Olá mundo!" > index.xhtml
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "docker_container" "ec2_simulada" {
+  name  = "fake-ec2-instance"
+  image = "python:3.9-alpine"
+  ports {
+    internal = var.server_port
+    external = var.server_port
+  }
+
+  entrypoint = ["/bin/sh", "-c"]
+  command = [
+    "mkdir -p /www && echo 'Olá mundo!' > /www/index.html && cd /www && python3 -m http.server ${var.server_port}"
+  ]
 }
